3fd906a78789047424c1a20eff0e79caaeeb2143
SHUBHAM2775 <shubhamu1332@gmail.com>
2025-03-20 01:05:41 +0530

    initial

--- resto.py
+++ resto.py
@@ -37,5 +37,4 @@ menu.set("Select A Restaurant")
 drop = ttk.Combobox(root, textvariable=menu, values=["Resto1","Resto2"], font=('Georgia', 18), justify="center", state = "readonly")
 drop.place(x=600, y=200, width=300, height=40)
 
-test = tk.Entry (root, font="Arial")
 root.mainloop()
